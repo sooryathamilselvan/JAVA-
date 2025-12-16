febeaa91b22cc17bfaacd49af78e67e7cd52c690
Soorya KT <sooryathamilselvan@gmail.com>
2025-12-16 13:19:00 +0530

    Updated code for Reverse a number problem.

--- 13-12-25/reverseanumber.java
+++ 13-12-25/reverseanumber.java
@@ -1,39 +1,16 @@
 import java.util.Scanner;
 
 public class reverseanumber {
-    public static long reverseNumber(long n) {
-        long sign = n < 0 ? -1 : 1;
-        n = Math.abs(n);
-        long rev = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev = 0;
+
         while (n > 0) {
             rev = rev * 10 + (n % 10);
-            n /= 10;
-        }
-        return rev * sign;
-    }
-
-    public static void main(String[] args) {
-        long number;
-        if (args.length > 0) {
-            try {
-                number = Long.parseLong(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid number: " + args[0]);
-                return;
-            }
-        } else {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter an integer: ");
-            if (!sc.hasNextLong()) {
-                System.err.println("No valid integer provided.");
-                sc.close();
-                return;
-            }
-            number = sc.nextLong();
-            sc.close();
+            n = n / 10;
         }
 
-        long reversed = reverseNumber(number);
-        System.out.println("Reversed: " + reversed);
+        System.out.println(rev);
     }
 }
