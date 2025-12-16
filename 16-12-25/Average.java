e79c3433e3d1afd7feeab7e512625415eb3c166e
Soorya KT <sooryathamilselvan@gmail.com>
2025-12-16 14:20:13 +0530

    Average of the elements in an array.

---
+++ 16-12-25/Average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Average{
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int []a=new int[n];
+        int sum=0;
+        for(int i=0; i<n ; i++){
+            a[i]=sc.nextInt();
+            sum+=a[i];
+        }
+        double avg=(double) sum/n;
+        System.out.println("Average= " + avg);
+    }
+}
