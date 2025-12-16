c467762f8e11f45497dd98804e7802422b66b190
Soorya KT <sooryathamilselvan@gmail.com>
2025-12-16 13:16:29 +0530

    Made a simpler code for fibonacci series.

--- 13-12-25/fibonacci.java
+++ 13-12-25/fibonacci.java
@@ -2,39 +2,15 @@ import java.util.Scanner;
 
 public class fibonacci {
     public static void main(String[] args) {
-        int n;
-        if (args.length > 0) {
-            try {
-                n = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid argument. Provide a positive integer.");
-                return;
-            }
-        } else {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter number of terms: ");
-            if (!sc.hasNextInt()) {
-                System.err.println("Please enter a valid positive integer.");
-                sc.close();
-                return;
-            }
-            n = sc.nextInt();
-            sc.close();
-        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        if (n <= 0) {
-            System.out.println("No terms to display.");
-            return;
-        }
-
-        long a = 0, b = 1;
+        int a = 0, b = 1;
         for (int i = 1; i <= n; i++) {
-            System.out.print(a);
-            if (i < n) System.out.print(" ");
-            long next = a + b;
+            System.out.print(a + " ");
+            int c = a + b;
             a = b;
-            b = next;
+            b = c;
         }
-        System.out.println();
     }
 }
