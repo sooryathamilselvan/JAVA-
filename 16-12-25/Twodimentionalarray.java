d17a6913a57fce5468d05714a694cc0b0a5112a7
Soorya KT <sooryathamilselvan@gmail.com>
2025-12-16 14:36:57 +0530

    Two dimentional array problem.

---
+++ 16-12-25/Twodimentionalarray.java
@@ -0,0 +1,13 @@
+class Twodimentionalarray{
+    public static void main(String[] args){
+		int a[][]={{1,2,3},{7,8,9},{10,11,12}};
+		for(int i=0; i<3; i++)
+		{
+		    for(int j=0; j<3; j++){
+		      System.out.print(a[i][j]+" ");
+		    }
+		    System.out.println();
+		}
+
+	}
+}
