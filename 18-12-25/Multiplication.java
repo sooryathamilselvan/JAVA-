65e4c6733eac36ae12bfbc2218b92ed5012d49bc
Soorya KT <sooryathamilselvan@gmail.com>
2025-12-18 22:07:11 +0530

    Matrix Multiplication.

---
+++ 18-12-25/Multiplication.java
@@ -0,0 +1,37 @@
+public class Multiplication {
+    
+    public static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) {
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
+        
+        int[][] result = new int[rowsA][colsB];
+        
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrixA = {{1, 2}, {3, 4}};
+        int[][] matrixB = {{5, 6}, {7, 8}};
+        
+        int[][] result = multiplyMatrices(matrixA, matrixB);
+        printMatrix(result);
+    }
+}
